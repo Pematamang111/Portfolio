4f8d62063425e34477e8533d4eebe512e6af367f
Pema Sherpa <pematamang111@gmail.com>
2024-05-07 00:22:23 -0400

    formatted all files

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 //import './nav.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css'
+import './style.css';
 import './components/nav.css';
 
-
-
-import App from './App.jsx'
+import App from './App.jsx';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 import ErrorPage from './pages/ErrorPage.jsx';
@@ -17,31 +15,29 @@ import HomePage from './pages/HomePage.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 
-
 const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          index: true,
-          element: <HomePage />,
-        },
-        {
-          path: 'profile',
-          element: <ProfilePage />,
-        },
-        {
-          path: 'about',
-          element: <AboutPage />,
-        },
-      ],
-    },
-  ]);
-  
-  // Render the RouterProvider component
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
-  );
-  
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				index: true,
+				element: <HomePage />,
+			},
+			{
+				path: 'profile',
+				element: <ProfilePage />,
+			},
+			{
+				path: 'about',
+				element: <AboutPage />,
+			},
+		],
+	},
+]);
+
+// Render the RouterProvider component
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<RouterProvider router={router} />
+);

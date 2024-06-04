c0d71c7d34f50c247082de6bc85f0839e22706f8
Pema Sherpa <pematamang111@gmail.com>
2024-06-04 18:36:00 -0400

    send email ref added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,17 +2,43 @@ import './contact.css';
 import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+	const form = useRef();
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+				publicKey: 'YOUR_PUBLIC_KEY',
+			})
+			.then(
+				() => {
+					console.log('SUCCESS!');
+				},
+				(error) => {
+					console.log('FAILED...', error.text);
+				}
+			);
+	};
+
 	return (
 		<div className="col-12 col-md-8 mb-3 form">
-			<h1 className='title'>Contact Me</h1>
-			<p className='descrip'>Please fill out the form below to discuss any work oppertunities.</p>
-			<form action="">
-				<input type="text" className="name" placeholder="Your Name" required/>
-				<input type="text" className="emailInput" placeholder="Your Email" required/>
+			<h1 className="title">Contact Me</h1>
+			<p className="descrip">
+				Please fill out the form below to discuss any work oppertunities.
+			</p>
+			<form action="" ref={form}>
+				<input type="text" className="name" placeholder="Your Name" required />
+				<input
+					type="text"
+					className="emailInput"
+					placeholder="Your Email"
+					required
+				/>
 				<textarea
 					name="message"
 					className="msg"
-					placeholder="Your Message" required></textarea>
+					placeholder="Your Message"
+					required></textarea>
 				<button type="submit" value="Send" className="submitbtn">
 					Submit
 				</button>
